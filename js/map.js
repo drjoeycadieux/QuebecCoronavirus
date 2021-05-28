9347fa6483070fab54a1ed8dcb38e0ebdbacc171
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-05-28 14:47:43 +0000

    index map

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@ var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    attribution: '&copy; The COVID-19 Canada <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
