a2dd91338410bdfdffe97c07151c1a7d16c28806
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-05-28 14:41:40 +0000

    Adding leaflet.js map

--- js/map.js
+++ js/map.js
@@ -1,57 +1,11 @@
-const mapbox_token =
-    "pk.eyJ1Ijoiam9leWNyZWF0b3IiLCJhIjoiY2tva253dHRxMDFhMzJubzF0NmNidTV1byJ9.fuQQQ11Nb0tnr-jbWemOsQ";
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-mapboxgl.accessToken = mapbox_token;
 
-var map = new mapboxgl.Map({
-    container: "map",
-    style: "mapbox://styles/mapbox/light-v10",
-    center: [- 73.5673, 45.5017],
-    zoom: 8.0
-});
-
-map.scrollZoom.disable();
-
-map.on('load', function() {
-    // Add a data source containing GeoJSON data.
-    map.addSource('covid-data', {
-        'type': 'geojson',
-        'data': {
-            'type': 'Feature',
-            'geometry': {
-                'type': 'Polygon',
-                // These coordinates outline Maine.
-                'coordinates': [
-                    [
-                        [- 73.5673, 45.5017],
-                        [-73.7990, 45.4720],
-                        [-73.8206, 45.4896]
-                    ]
-                ]
-            }
-        }
-    });
-
-    // Add a new layer to visualize the polygon.
-    map.addLayer({
-        'id': 'covid-data',
-        'type': 'fill',
-        'source': 'covid-data', // reference the data source
-        'layout': {},
-        'paint': {
-            'fill-color': 'red', // blue color fill
-            'fill-opacity': 0.5
-        }
-    });
-    // Add a black outline around the polygon.
-    map.addLayer({
-        'id': 'outline',
-        'type': 'line',
-        'source': 'covid-data',
-        'layout': {},
-        'paint': {
-            'line-color': '#000',
-            'line-width': 3
-        }
-    });
-});
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoiam9leWNyZWF0b3IiLCJhIjoiY2tva253dHRxMDFhMzJubzF0NmNidTV1byJ9.fuQQQ11Nb0tnr-jbWemOsQ'
+}).addTo(mymap);
