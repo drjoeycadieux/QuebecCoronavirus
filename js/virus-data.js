94540d1bfb3cbc543bb25db2c473df9d213219c8
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-06-14 13:00:14 +0000

    doing some work

---
+++ js/virus-data.js
@@ -0,0 +1,45 @@
+const virusData = [
+    // status: 1 -> up
+    // status: 0 -> holding
+    {
+        id: 42001,
+        county: 'Quebec',
+        cases: 6,
+        status: 0,
+        dateOfFirstCase: '2020-06-19',
+        dateOfDisasterDeclaration: null,
+        dailyOverview: [
+            {
+                date: '2020-06-18',
+                cases: 0,
+            },
+            {
+                date: '2020-06-19',
+                cases: 1,
+            },
+            {
+                date: '2020-06-20',
+                cases: 4,
+            },
+            {
+                date: '2020-06-21',
+                cases: 4,
+            },
+            {
+                date: '2020-03-22',
+                cases: 5,
+            },
+            {
+                date: '2020-03-23',
+                cases: 6,
+            },
+            {
+                date: '2020-03-24',
+                cases: 6,
+            },
+            {
+                date: '2020-03-25',
+                cases: 6,
+            },
+        ]
+          export default virusData
