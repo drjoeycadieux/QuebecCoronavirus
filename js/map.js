5e52ca8f96f601771b661e089c0cbde899a1a806
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-06-18 16:47:47 +0000

    push

---
+++ js/map.js
@@ -0,0 +1,14 @@
+fetch('https://api.covid19api.com/countries')
+.then(res => console.log(res))
+
+
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoiam9leWNyZWF0b3IiLCJhIjoiY2tva253dHRxMDFhMzJubzF0NmNidTV1byJ9.fuQQQ11Nb0tnr-jbWemOsQ';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/dark-v10',
+  center: [-96.052335, 39.159882],
+  zoom: 4.5
+});
+
+map.scrollZoom.disable();
