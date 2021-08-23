e30d0128fe9bc5e4449742be05414d8edd21df83
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-08-23 16:20:10 -0400

    removing some garbage file.

--- js/console.js
+++ js/console.js
@@ -17,7 +17,3 @@ console.log(
   "%cUsing this console may allow attackers to impersonate you and steal your information using an attack called Self-XSS.Do not enter or paste code that you do not understand.",
   cssRule
 );
-
-function learnFunction() {
-    alert(window.location.ancestorOrigins[0]);
-}
--- js/map.js
+++ js/map.js
@@ -1,8 +1,20 @@
-mapboxgl.accessToken = 'pk.eyJ1Ijoiam9leWNyZWF0b3IiLCJhIjoiY2tva253dHRxMDFhMzJubzF0NmNidTV1byJ9.fuQQQ11Nb0tnr-jbWemOsQ';
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/dark-v10',
-  center: [-96.052335, 39.159882],
-  zoom: 4.5
-});
-
+const chart = document.getElementById("chart-async-data-example");
+const chartInstance = new mdb.Chart(chart, { type: "bar" });
+fetch("https://api.covid19api.com/summary")
+  .then((data) => data.json())
+  .then((data) => {
+    const dataset = data.map((obj) => obj["Cases"]);
+    const labels = data.map((obj) => obj["Date"].substr(0, 10));
+    chartInstance.update({
+      labels,
+      datasets: [
+        {
+          color: "#FFCDD2",
+          data: dataset,
+          label: "Number of cases",
+          pointRadius: 0,
+          borderColor: dataset.color
+        }
+      ]
+    });
+  });
